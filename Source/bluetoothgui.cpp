6b96f6a3dd1b4524b06334a668584dcba46df07a
Tim Fornell <tim.fornell@hotmail.com>
2018-06-30 22:55:49 +0200

    Started implementing bluetooth connection

--- Source/bluetoothgui.cpp
+++ Source/bluetoothgui.cpp
@@ -6,9 +6,46 @@ BluetoothGUI::BluetoothGUI(QWidget *parent) :
     ui(new Ui::BluetoothGUI)
 {
     ui->setupUi(this);
+
+    connect(ui->search, SIGNAL (released()), this, SLOT (SearchForDevices()));
+    connect(ui->connect, SIGNAL (released()), this, SLOT (ConnectToDevice()));
+
+    // Check if Bluetooth is available on this device
+    if (localDevice.isValid()) {
+
+        // Turn Bluetooth on
+        localDevice.powerOn();
+
+        // Read local device name
+        localDeviceName = localDevice.name();
+
+        // Make it visible to others
+        localDevice.setHostMode(QBluetoothLocalDevice::HostDiscoverable);
+
+        // Get connected devices
+        remotes = localDevice.connectedDevices();
+    }
 }
 
 BluetoothGUI::~BluetoothGUI()
 {
     delete ui;
+}
+
+// Slots
+void BluetoothGUI::SearchForDevices()
+{
+    // Clear list
+    ui->devices->clear();
+
+    for(int i = 0; i < 5; i++){
+        QString s = "Testing";
+        ui->devices->addItem(s);
+    }
+}
+
+void BluetoothGUI::ConnectToDevice()
+{
+    int index = ui->devices->currentIndex();
+    qDebug("Index: %d", index);
 }
--- Source/bluetoothgui.h
+++ Source/bluetoothgui.h
@@ -2,6 +2,7 @@
 #define BLUETOOTHGUI_H
 
 #include <QMainWindow>
+#include <QtBluetooth>
 
 namespace Ui {
 class BluetoothGUI;
@@ -15,11 +16,19 @@ public:
     explicit BluetoothGUI(QWidget *parent = 0);
     ~BluetoothGUI();
 
-    // Member functions
-
+public slots:
+    void SearchForDevices();
+    void ConnectToDevice();
 
 private:
     Ui::BluetoothGUI *ui;
+    // Host info
+    QBluetoothLocalDevice localDevice;
+    Qstring localDevice;
+    // Nearby devices
+    Qlist<QBluetoothAddress> nearbyDevices;
+    // Connected devices
+    Qlist<QBluetoothAddress> remotes;
 };
 
 #endif // BLUETOOTHGUI_H
